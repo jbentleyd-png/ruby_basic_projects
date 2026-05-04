708c53abdeeda816720d55fb8ff5c2875d79ac03
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-05-04 10:25:25 -0600

    Finished caesar cipher

---
+++ cc.rb
@@ -0,0 +1,44 @@
+def shifty(current_character, ref_array, shift_value)
+  old_index = nil
+  ref_array.each_with_index do |character, index|
+    if character == current_character
+      old_index = index
+    end
+  end
+
+  if old_index == nil
+    return current_character
+  else 
+    new_index = old_index + shift_value
+    if new_index > 25
+      new_index -= 26
+    end
+    ref_array[new_index]
+  end
+end
+
+
+
+def caesar_cipher(any_string, shift_value)
+  downcase_letters = ('a'..'z').to_a
+  uppercase_letters = ('A'..'Z').to_a
+  ref_array =nil
+  
+  chopped_up_string = any_string.chars
+  shifted_array = []
+
+
+  chopped_up_string.each do |character|
+    if  character == character.downcase
+      ref_array = downcase_letters
+    elsif character == character.upcase
+      ref_array = uppercase_letters
+    end
+    shifted_array.push(shifty(character, ref_array, shift_value))
+
+  end
+  shifted_array.join
+end
+
+puts caesar_cipher("What a string!", 5)
+
