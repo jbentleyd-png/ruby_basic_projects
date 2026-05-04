edbf04a3ab0b3feb203cb4f7229f1f9d246cb991
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-05-04 16:23:00 -0600

    Wrote macro logic for bubble_sort
    No swapping yet, just conditions and efficiency.

---
+++ bubble_sort.rb
@@ -0,0 +1,20 @@
+def bubble_sort(array)
+  for macro_count in 0...(array.length - 1) # can run max array.length - 1 times
+    swapped = false
+
+    for micro_count in 0...(array.length - macro_count - 1) # top number should be sorted by first macro loop, 2nd highest by second, etc. 
+      
+    end
+
+    if swapped == false
+      return array
+    end
+
+
+  end
+  array # return if max cycles complete
+end
+
+
+bubble_sort([4,3,78,2,0,2])
+
